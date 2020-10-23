b4b38d5476cc24862fbc7af16547c7a25d11c2d2
NoLevL <59285504+NoLevL@users.noreply.github.com>
2020-10-23 11:04:44 +0200

    Category

--- Models/Category.cs
+++ Models/Category.cs
@@ -19,10 +19,50 @@ namespace Models
                 sw.WriteLine(category);
             }
         }
+
+        public void DeleteCategory(string category)
+        {
+            categories.Remove(category);
+            UpdateCategories(categories);
+        }
+
+        public void EditCategory(string categoryToEdit, string newName)
+        {
+            int indexOfCat = categories.IndexOf(categoryToEdit);
+            categories[indexOfCat] = newName;
+            UpdateCategories(categories);
+        }
+
+        public List<string> GetCategories()
+        {
+            categories.Clear();
+            if (File.Exists(savedCategories))
+            {
+                using (StreamReader sr = new StreamReader(savedCategories))
+                {
+                    string category = "";
+                    while (sr.ReadLine() != null)
+                    {
+                        category = sr.ReadLine();
+                        categories.Add(category);
+                    }
+                }
+            }
+            return categories;
+        }
+
+        public void UpdateCategories(List<string> listOfCategories)
+        {
+            File.WriteAllText(savedCategories, string.Empty);
+            using (StreamWriter sw = File.AppendText(savedCategories))
+            {
+                foreach (var cat in listOfCategories)
+                {
+                    sw.WriteLine(cat);
+                }
+            }
+        }
     }
 
-    public void UpdateCategories (List<string> listOfCategories)
-    {
-
-    }
+    
 }
