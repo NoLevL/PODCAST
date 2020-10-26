98c0c4546ea55ae375003c1668fd1133ee1fd282
Johannes <johannes_arnelund@hotmail.com>
2020-10-26 13:54:42 +0100

    Combobox och Listbox fill()

---
+++ PodcastApp/FormHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Models;
+
+namespace PodcastApp
+{
+    public class FormHandler
+    {
+        public static void ClearText(TextBox textBox)
+        {
+            textBox.Text = "";
+        }
+
+        public static void FillCategoryList(List<Category> list, ListBox listBox)
+        {
+            listBox.Items.Clear();
+            foreach (var item in list)
+            {
+                listBox.Items.Add(item.Name);
+            }
+        }
+
+        public static void FillCategoryComboBox(List<Category> list, ComboBox comboBox)
+        {
+            comboBox.Items.Clear();
+            foreach (var item in list)
+            {
+                comboBox.Items.Add(item.Name);
+            }
+        }
+    }
+}
