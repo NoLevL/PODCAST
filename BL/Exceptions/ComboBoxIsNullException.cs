446082207a7181354dc8b962bfc5c97a625d4b6e
NoLevL <59285504+NoLevL@users.noreply.github.com>
2020-10-26 14:00:12 +0100

    Exception

---
+++ BL/Exceptions/ComboBoxIsNullException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Exceptions
+{
+    class ComboBoxIsNullException : Exception
+    {
+
+        public ComboBoxIsNullException()
+            : base()
+        { }
+
+
+        public ComboBoxIsNullException(string message)
+            : base(message)
+        { }
+    }
+}
